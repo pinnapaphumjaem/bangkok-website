8a741b3cd95af2b4dec7f429dd56bf23a83a9fd4
pinnapaphumjaem <pinnapa.phumjaem@g.swu.ac.th>
2026-03-30 19:14:22 +0700

    Update language.js

--- language.js
+++ language.js
@@ -1,21 +1,71 @@
-function setLanguage(lang) {
-  localStorage.setItem("lang", lang);
-  applyLanguage(lang);
-}
+const text = {
+  en: {
+    title_home: "MY CONCIERGE",
+    historical: "Historical",
+    mall: "Mall",
+    park: "Park",
+    market: "Market",
+    restaurant: "Restaurant",
 
-function applyLanguage(lang) {
-  if (lang === "jp") {
-    document.querySelectorAll("[data-en]").forEach(el => {
-      el.innerText = el.getAttribute("data-jp");
-    });
-  } else {
-    document.querySelectorAll("[data-en]").forEach(el => {
-      el.innerText = el.getAttribute("data-en");
-    });
+    // park
+    lumpini: "Lumpini Park",
+    benjakitti: "Benjakitti Park",
+    chatuchak: "Chatuchak Park",
+    rotfai: "Wachirabenchathat Park",
+    benchasiri: "Benchasiri Park",
+
+    // restaurant
+    gov: "THE GOVERNOR",
+    baan: "Baan Khanitha",
+    local: "The Local",
+    manho: "Man Ho",
+    indus: "Indus",
+    sala: "Sala Rim Naam",
+    shakarik: "Shakarik"
+  },
+
+  jp: {
+    title_home: "コンシェルジュ",
+    historical: "歴史",
+    mall: "ショッピングモール",
+    park: "公園",
+    market: "マーケット",
+    restaurant: "レストラン",
+
+    // park
+    lumpini: "ルンピニー公園",
+    benjakitti: "ベンジャキティ公園",
+    chatuchak: "チャトゥチャック公園",
+    rotfai: "ワチラベンチャタート公園",
+    benchasiri: "ベンチャシリ公園",
+
+    // restaurant
+    gov: "ザ・ガバナー",
+    baan: "バーン・カニタ",
+    local: "ザ・ローカル",
+    manho: "マンホー",
+    indus: "インダス",
+    sala: "サラ・リム・ナーム",
+    shakarik: "シャカリキ"
   }
+};
+
+function setLang(lang) {
+  localStorage.setItem("lang", lang);
+  applyLang(lang);
 }
 
+function applyLang(lang) {
+  document.querySelectorAll("[data-key]").forEach(el => {
+    const key = el.getAttribute("data-key");
+    if (text[lang][key]) {
+      el.innerText = text[lang][key];
+    }
+  });
+}
+
+// โหลดอัตโนมัติทุกหน้า
 document.addEventListener("DOMContentLoaded", function () {
   const lang = localStorage.getItem("lang") || "en";
-  applyLanguage(lang);
+  applyLang(lang);
 });
