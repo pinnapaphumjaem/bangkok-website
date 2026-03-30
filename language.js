e1d09bff9d1fd1a326c92792122ad01f12c9dcc8
pinnapaphumjaem <pinnapa.phumjaem@g.swu.ac.th>
2026-03-30 19:18:48 +0700

    Update language.js

--- language.js
+++ language.js
@@ -1,20 +1,19 @@
 const text = {
   en: {
     title_home: "MY CONCIERGE",
-    historical: "Historical",
-    mall: "Mall",
-    park: "Park",
-    market: "Market",
-    restaurant: "Restaurant",
+    hotel: "HILTON BANGKOK GRANDE ASOKE",
+    historical: "📍 Historical",
+    mall: "🛍 Mall",
+    park: "🌳 Park",
+    market: "🛒 Market",
+    restaurant: "🍽 Restaurant",
 
-    // park
     lumpini: "Lumpini Park",
     benjakitti: "Benjakitti Park",
     chatuchak: "Chatuchak Park",
     rotfai: "Wachirabenchathat Park",
     benchasiri: "Benchasiri Park",
 
-    // restaurant
     gov: "THE GOVERNOR",
     baan: "Baan Khanitha",
     local: "The Local",
@@ -26,20 +25,19 @@ const text = {
 
   jp: {
     title_home: "コンシェルジュ",
-    historical: "歴史",
-    mall: "ショッピングモール",
-    park: "公園",
-    market: "マーケット",
-    restaurant: "レストラン",
+    hotel: "ヒルトン バンコク グランデ アソーク",
+    historical: "📍 歴史スポット",
+    mall: "🛍 ショッピングモール",
+    park: "🌳 公園",
+    market: "🛒 マーケット",
+    restaurant: "🍽 レストラン",
 
-    // park
     lumpini: "ルンピニー公園",
     benjakitti: "ベンジャキティ公園",
     chatuchak: "チャトゥチャック公園",
     rotfai: "ワチラベンチャタート公園",
     benchasiri: "ベンチャシリ公園",
 
-    // restaurant
     gov: "ザ・ガバナー",
     baan: "バーン・カニタ",
     local: "ザ・ローカル",
@@ -64,7 +62,6 @@ function applyLang(lang) {
   });
 }
 
-// โหลดอัตโนมัติทุกหน้า
 document.addEventListener("DOMContentLoaded", function () {
   const lang = localStorage.getItem("lang") || "en";
   applyLang(lang);
