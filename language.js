b855ce6624234bfb3aa11741fdb5bb3719036047
pinnapaphumjaem <pinnapa.phumjaem@g.swu.ac.th>
2026-03-31 22:26:58 +0700

    Update language.js

--- language.js
+++ language.js
@@ -1,6 +1,13 @@
 const text = {
 
 en:{
+title:"MY CONCIERGE",
+hotel:"HILTON BANGKOK GRANDE ASOKE",
+historical:"Historical",
+mall:"Mall",
+park:"Park",
+market:"Market",
+restaurant:"Restaurant",
 phra_title:"WAT PHRA KAEW",
 highlight:"Highlight",
 phra_desc:"Wat Phra Kaew is the most sacred temple in Thailand and a cornerstone of the nation’s spiritual and cultural identity. Located within the grounds of The Grand Palace, the temple houses the revered Emerald Buddha, a powerful symbol of prosperity, protection, and royal legitimacy. The statue, carved from a single block of jade, is highly respected by Thai people, and its seasonal costume changes are performed by the King in an important royal ceremony. The temple is also famous for its exquisite Ramakien murals that decorate the surrounding gallery walls. These detailed paintings illustrate scenes from Thailand’s national epic, portraying heroic battles, mythical beings, and moral lessons that reflect Thai beliefs and cultural values. With its dazzling golden architecture, intricate craftsmanship, and deep spiritual significance, Wat Phra Kaew is one of the most extraordinary cultural treasures in Thailand.",
@@ -14,6 +21,13 @@ googlemaps:"Google Maps"
 },
 
 jp:{
+title:"マイ・コンシェルジュ",
+hotel:"ヒルトン・バンコク・グランデ・アソーク",
+historical:"歴史的名所",
+mall:"ショッピングモール",
+park:"公園",
+market:"マーケット",
+restaurant:"レストラン",
 phra_title:"ワット・プラケオ",
 highlight:"見どころ",
 phra_desc:"ワット・プラケオはタイで最も神聖な寺院であり、国家の精神的および文化的アイデンティティの礎となっています。王宮（グランドパレス）の敷地内に位置し、この寺院には繁栄、守護、そして王権の正当性を象徴するエメラルド仏が安置されています。この仏像は一つの翡翠の塊から彫られており、タイの人々から非常に深い尊敬を集めています。また、季節ごとに衣装を替える儀式は国王によって執り行われる重要な王室儀礼です。
